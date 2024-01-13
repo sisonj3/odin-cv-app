3abd70393f2394120f6720828168876a001c73d7
sisonj <98570174+sisonj3@users.noreply.github.com>
2024-01-13 15:20:33 -0800

    Created CV.jsx and added CV to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Form from './components/Form'
+import CV from './components/CV'
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Form />
+      <CV />
     </>
   )
 }
---
+++ src/components/CV.jsx
@@ -0,0 +1,9 @@
+function CV() {
+    return (
+        <div>
+            <h1>CV info here</h1>
+        </div>
+    );
+}
+
+export default CV;
