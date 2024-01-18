2c64ba031d4e36903f6d0dce189e0baeb5250f46
sisonj <98570174+sisonj3@users.noreply.github.com>
2024-01-18 15:33:39 -0800

    Changed render of CV.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,30 @@ import CV from './components/CV'
 import './App.css'
 
 function App() {
+  // Personal Info States
   const [name, setName] = useState('First Last');
   const [email, setEmail] = useState('notReal@mail.com');
   const [phone, setPhone] = useState('555-555-5555');
   const [address, setAddress] = useState('1234 Main St');
+
+  // Education state
+  const [education, setEducation] = useState([
+    {
+      school: 'UNLV',
+      degree: 'Associates',
+      city: 'Las Vegas',
+      country: 'USA',
+      startDate: new Date(2017, 8, 28),
+      endDate: new Date(2019, 12, 16)
+    },
+    {
+      school: 'CSN',
+      degree: 'Bachelors',
+      city: 'Las Vegas',
+      country: 'USA',
+      startDate: new Date(2019, 8, 28),
+      endDate: new Date(2022, 12, 16)
+    }]);
 
   let getFormData = (formName, formEmail, formPhone, formAddress) => {
     setName(formName);
--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -1,12 +1,28 @@
 function CV({ cvName, cvEmail, cvPhone, cvAddress,
-            cvEducation, cvExperience}) {
+    cvEducation, cvExperience }) {
+    
+    const cvDiv = document.createElement('div');
+
+    // Personal info
+    const nameP = document.createElement('p');
+    const emailP = document.createElement('p');
+    const phoneP = document.createElement('p');
+    const addressP = document.createElement('p');
+
+    // Set personal info
+    nameP.textContent = cvName;
+    emailP.textContent = cvEmail;
+    phoneP.textContent = cvPhone;
+    addressP.textContent = cvAddress;
+    
+    // Add Personal info to cvDiv
+    cvDiv.appendChild(nameP);
+    cvDiv.appendChild(emailP);
+    cvDiv.appendChild(phoneP);
+    cvDiv.appendChild(addressP);
+
     return (
-        <div>
-            <p>{cvName}</p>
-            <p>{cvEmail}</p>
-            <p>{cvPhone}</p>
-            <p>{cvAddress}</p>    
-        </div>
+        <div ref={(nodeElement) => {nodeElement && nodeElement.appendChild(cvDiv)}}/>
     );
 }
 
